24e6b668b8e5351a6fe9c20c801b3fb47f236964
RITAM CHARAN <ritamcharan232001@gmail.com>
2022-05-29 17:16:54 +0530

    Update admin_auth.js

--- js/admin/admin_auth.js
+++ js/admin/admin_auth.js
@@ -56,11 +56,11 @@ onAuthStateChanged(auth, (user) => {
     //bla bla bla
     // ...
   }
-  // else {
-  //   alert("You should login first!!!!");
-  //   window.location = "../registration.html";
-  //   // User is signed out
-  //   // ...
-  //   //bla bla bla
-  // }
+  if (!user) {
+    alert("You should login first!!!!");
+    window.location = "../registration.html";
+    // User is signed out
+    // ...
+    //bla bla bla
+  }
 });
